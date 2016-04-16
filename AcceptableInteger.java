3c295e635a01c2a09c3f20c58ccaf7a2069462f9
Charlesexum1 <cexum@uga.edu>
2016-04-16 10:13:29 -0400

    Added files via upload

---
+++ AcceptableInteger.java
@@ -0,0 +1,46 @@
+/*
+ * AcceptibleInteger.java
+ * 
+ * Copyright 2016 graf <graf@chillosphere.spacepotato>
+ * 
+ * 
+ * 
+ */
+import java.util.Scanner;
+
+public class AcceptableInteger {
+		
+	private int minAcceptable = 1;
+	private int maxAcceptable = 10;
+	private String prompt = "type a numer between 1 and to";
+	private int userInput;
+	
+	public void getValue() {
+		int input;
+		Scanner keys = new Scanner(System.in);
+		System.out.println(prompt);
+		input = keys.nextInt();
+		userInput = input;
+		System.out.println("you input " + userInput);		
+		}
+	
+	public void checkValidity() {
+		if (userInput >= 1 && userInput <= 10) {
+			System.out.println(userInput + " is valid");
+			}
+		else {
+			System.out.println(userInput + " is not valid");
+			}	
+		}
+			
+	public static void main (String args[]) {
+		
+		AcceptableInteger test = new AcceptableInteger();
+		test.getValue();
+		test.checkValidity();
+		
+		
+		
+	}
+}
+
